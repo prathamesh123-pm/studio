939fa37e07fb369e18a988cb2a951f22b6b5897d
prathamesh more <morep858@gmail.com>
2026-03-20 03:27:34 +0000

    संपूर्ण मास्टर ब्रँड रिपोर्ट या मधील कॉलम ची साईझ adjust करा

--- src/app/surveys/page.tsx
+++ src/app/surveys/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Navbar } from "@/components/layout/Navbar";
-import { useSurveyStore, SurveyRecord } from "@/lib/survey-store";
+import { useSurveyStore, SurveyRecord } from "@/survey-store";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
